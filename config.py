93945b447eb795b0e23669d857d0a742adf86b52
PYTICHO <116948476+PYTICHO@users.noreply.github.com>
2023-03-07 21:22:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "6045116877:AAG0MRuvju_rwgfQTi3cHAd-8CLO5UMa5uw"
+TOKEN = "Your TG Bot TOKEN"
 
 client_id = "87210588-03ba-4b3f-afcd-7b582102257f"
 client_secret = "6E2sxoP1PNUI6wU53hoYZBo1QjPjE52rjGPaFQZj0D6p3EIRl9jbdlJiuMtwIFaM"
