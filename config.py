36f373c428cd4c5aeaf42016f690dd9796b705e6
PYTICHO <116948476+PYTICHO@users.noreply.github.com>
2023-03-11 15:43:08 +0300

    fgr

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TOKEN = "YourTGBOTToken"
+TOKEN = "6045116877:AAEgaYr9qQGajniOCu8v8CltPvnxAZrpQz0"
 
-client_id = "87210588-03ba-4b3f-afcd-7b582102257f"
-client_secret = "6E2sxoP1PNUI6wU53hoYZBo1QjPjE52rjGPaFQZj0D6p3EIRl9jbdlJiuMtwIFaM"
-subdomain = "tyz20797"
+client_id = "dbb6d667-780b-4920-a8c7-de640f1c19f3"
+client_secret = "o2VXYgqTSVjf18TknFT0Xsp2A7WLv08fdtHOtMRSyEkkRe5D2Z2AUrGjAyq5oKWT"
+subdomain = "asiashadrin"
 redirect_url = "https://example.com"
