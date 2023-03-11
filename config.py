702a7b52f5c36f5feb97219b7ba3cf36a0dd9341
PYTICHO <kasaev_2006@inbox.ru>
2023-03-11 12:28:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN = "6045116877:AAEgaYr9qQGajniOCu8v8CltPvnxAZrpQz0"
+TOKEN = "YourTGBOTToken"
 
 client_id = "87210588-03ba-4b3f-afcd-7b582102257f"
 client_secret = "6E2sxoP1PNUI6wU53hoYZBo1QjPjE52rjGPaFQZj0D6p3EIRl9jbdlJiuMtwIFaM"
