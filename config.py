8c7f25a91e12a037b06f1709fded27c00b72ec1e
PYTICHO <kasaev_2006@inbox.ru>
2023-03-14 16:46:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TOKEN = "6045116877:AAEgaYr9qQGajniOCu8v8CltPvnxAZrpQz0"
+TOKEN = "TgToken"
 
-client_id = "dbb6d667-780b-4920-a8c7-de640f1c19f3"
-client_secret = "o2VXYgqTSVjf18TknFT0Xsp2A7WLv08fdtHOtMRSyEkkRe5D2Z2AUrGjAyq5oKWT"
+client_id = "ClientIdAmoCrm"
+client_secret = "ClientSecretAmoCrm"
 subdomain = "asiashadrin"
 redirect_url = "https://example.com"
